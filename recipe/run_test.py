42842250669b732af75b22462e936166fa35bd5a
ocefpaf <ocefpaf@gmail.com>
2017-01-27 15:43:41 -0300

    Add patches and re-render.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,9 +1,5 @@
 from pydap.client import open_url
 
-# OPeNDAP.
-# TODO: charset issue with webop.
-#url = 'http://geoport-dev.whoi.edu/thredds/dodsC/estofs/atlantic'
-# works for now
-# gunicorn
-url = 'http://tds.marine.rutgers.edu/thredds/dodsC/roms/espresso/2013_da/his/ESPRESSO_Real-Time_v2_History_Best'
+url = 'http://geoport-dev.whoi.edu/thredds/dodsC/estofs/atlantic'
+
 ds = open_url(url)
